9f94afe08402f60e6ba9bfb67447beba7362bf81
Jincy P Janardhanan <jincyp7@gmail.com>
2022-07-21 23:18:19 +0530

    add unique together constraint

--- base/models.py
+++ base/models.py
@@ -8,3 +8,10 @@ class RecipeInstruction(models.Model):
     time_stamp = models.CharField(max_length=8)
     seq_no = models.IntegerField()
     instruction = models.CharField(max_length=200)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['r_id', 'seq_no'], name='unique_r_id_seq_no'
+            )
+        ]
